049a6d72141b282e81b7cb8756a1a07c034d4397
kanpah01 <undefined>
2019-11-13 17:50:19 +0000

    Changed to LED_RED in main.cpp file

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "platform/mbed_thread.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED_GREEN);
+DigitalOut led1(LED_RED);
 
 #define SLEEP_TIME                  1500 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
